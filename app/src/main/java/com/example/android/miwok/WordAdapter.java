c97aa2bf1bf66e7693506196f5d1b4e83f4e03fa
Davo Faulkner <d@davo.co>
2016-09-11 18:52:30 -0500

    Add additional WordAdapter Javadoc comments

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -17,6 +17,15 @@ public class WordAdapter extends ArrayAdapter<Word>{
         super(context, 0, words);
     }
 
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
