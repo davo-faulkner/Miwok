3ffe790a81ba4909943ce3945fbf6a48b57238ea
Davo Faulkner <d@davo.co>
2016-09-11 19:41:20 -0500

    Change name of activity_numbers.xml to word_list.xml and delete other layout files

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -26,7 +26,7 @@ public class PhrasesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<>();
         words.add(new Word("Where are you going?", "minto wuksus"));
