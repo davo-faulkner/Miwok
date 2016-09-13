aadda8e373e9d078677d62bc493dfc733efe917e
Davo Faulkner <d@davo.co>
2016-09-13 06:58:08 -0500

    Finish WordAdapter.java to show correct colors

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,28 +14,28 @@ import java.util.ArrayList;
 /**
  * Created by Davo on 9/11/2016.
  */
-public class WordAdapter extends ArrayAdapter<Word>{
-    private int mColorID;
+public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResourceId;
 
     public WordAdapter(Activity context, ArrayList<Word> words, int ColorID) {
         super(context, 0, words);
-        this.mColorID = ColorID;
+        this.mColorResourceId = ColorID;
     }
 
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
      *
-     * @param position The position in the list of data that should be displayed in the
-     *                 list item view.
+     * @param position    The position in the list of data that should be displayed in the
+     *                    list item view.
      * @param convertView The recycled view to populate.
-     * @param parent The parent ViewGroup that is used for inflation.
+     * @param parent      The parent ViewGroup that is used for inflation.
      * @return The View for the position in the AdapterView.
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
-        if(listItemView == null) {
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
@@ -60,6 +61,13 @@ public class WordAdapter extends ArrayAdapter<Word>{
         } else {
             imageView.setVisibility(View.GONE);
         }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.linear_layout);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews)
         // so that it can be shown in the ListView
